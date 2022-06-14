611a9d1bb04a272be73aad8c169e17d2f888d733
pagepc <82887638+pagepc@users.noreply.github.com>
2022-06-14 17:52:37 -0400

    Add files via upload

---
+++ simplePong.py
@@ -0,0 +1,142 @@
+"""
+Created on Tue Jun 14 15:58:14 2022
+
+@author: pagep
+"""
+
+import turtle 
+
+wn = turtle.Screen()
+wn.title("Pong by PP")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+# wn.tracer forces manual updates, without it game would run much slower
+wn.tracer(0)
+
+# keeping score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #speed of animation 
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) #speed of animation 
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(+350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0) #speed of animation 
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = .05 #every time ball moves, it moves by 2 pixals 
+ball.dy = .05
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("green")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Comic Sans MS", 24, "bold"))
+
+# Paddle Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20 #add 20 pixels to the y coord
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20 #add 20 pixels to the y coord
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20 #add 20 pixels to the y coord
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20 #add 20 pixels to the y coord
+    paddle_b.sety(y)
+
+# define keyboard binding
+wn.listen() #listen to keyboard input
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# main game loop (necessary for every game)
+while True:
+    wn.update() # eveytime loop runs it updates screen 
+    
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    # borders
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1 #reverses direction of ball 
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1 #reverses direction of ball 
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0) #puts ball back to center
+        ball.dx *= -1
+        score_a +=1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Comic Sans MS", 24, "bold"))
+        
+    if ball.xcor() < -390:
+        ball.goto(0, 0) #puts ball back to center
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Comic Sans MS", 24, "bold"))    
+    # paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+        
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
